048a18a7915d7df3a883e8c72c786a163a46f8cc
d10n <david@bitinvert.com>
2017-09-12 21:18:03 -0400

    Add test progress bar

--- scripts/generate-test-output.sh
+++ scripts/generate-test-output.sh
@@ -19,5 +19,26 @@ line "\rhel"
 line "\rfoo"
 line "\rfoobar\n"
 line "fooobar\n"
-line "\n"
+
+progressline() {
+    width=$(tput cols)
+    percent="$1"
+    filled=$(( width * percent / 100 ))
+    remainder=$(( width - filled ))
+    perl -e 'printf("\r"."#" x $ARGV[0] . "-" x $ARGV[1]);
+        select(undef, undef, undef, 0.25)' $filled $remainder
+}
+printf "\n"
+progressline 0
+progressline 10
+progressline 20
+progressline 30
+progressline 40
+progressline 50
+progressline 60
+progressline 70
+progressline 80
+progressline 90
+progressline 100
+printf "\n"
 )
